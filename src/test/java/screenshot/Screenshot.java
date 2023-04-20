01120084562ed2e261eece6b32b73a633ad45084
Abhinandan <abhinandan.maholkar@coditas.com>
2023-04-20 11:34:52 +0530

    Intial commit

---
+++ src/test/java/screenshot/Screenshot.java
@@ -0,0 +1,23 @@
+package screenshot;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Screenshot {
+    public static void main(String[] args) throws IOException {
+        ChromeOptions options=new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver=new ChromeDriver(options);
+        driver.get("https://people.zoho.in/");
+        File source=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(source, new File("C:\\Users\\coditas\\Pictures\\Screenshot\\zoho.png"));
+
+    }
+}
